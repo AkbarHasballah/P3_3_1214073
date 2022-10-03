71d9e072915b6d186df0155fb4cb00cc4f6939e4
AkbarHasballah <salmanakbartafisa@gmail.com>
2022-10-03 16:20:29 +0700

    Add project files.

---
+++ P3_3_1214073/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_3_1214073
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a >10 || a<= 0)
+            {
+                Console.WriteLine("angka tidak sesuai");
+            }
+            else
+            {
+                for(int n= a;n >0; n--)
+                    if(n== 1)
+                {
+                    Console.WriteLine("Anak Ayam Turunlah 1, mati satu tinggal induknya");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Anak Ayam turunlah" + n + "Mati satu tinggalah" + (n - 1));
+                    }
+            }
+        }
+    }
+}
